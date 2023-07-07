0eea3587e1222899e5367bd1c8387b217ff7a7ca
wb_zhuminghao <wb_zhuminghao@meituan.com>
2023-07-07 20:18:22 +0800

    feat: 删除无用依赖

--- src/types/module.d.ts
+++ src/types/module.d.ts
@@ -3,4 +3,3 @@ declare module 'three/examples/jsm/animation/MMDAnimationHelper.js';
 declare module 'three/examples/jsm/controls/OrbitControls.js';
 declare module 'three/examples/jsm/effects/OutlineEffect.js';
 declare module 'three/examples/jsm/loaders/MMDLoader.js';
-declare module '@docusaurus/BrowserOnly';
